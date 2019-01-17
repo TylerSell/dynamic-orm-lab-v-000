9bcced62a122405153115205bf69b9c635cce6d5
tylerselldesign <tylerselldesign@gmail.com>
2019-01-17 20:06:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,10 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.attributes
+    self::ATTRIBUTES 
+  end
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
