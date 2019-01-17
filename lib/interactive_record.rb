642ed5fb2409bd2239a0328db918714370570cd4
tylerselldesign <tylerselldesign@gmail.com>
2019-01-17 19:21:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,27 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+    
+  end
   
+  def self.column_names
+    
+  end
+  
+  def table_names_for_insert
+    
+  end
+  
+  def col_names_for_insert
+    
+  end
+  
+  def values_for_insert
+    
+  end
+  
+  def save
+    
+  end
 end
