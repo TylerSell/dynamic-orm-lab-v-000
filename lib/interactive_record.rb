a00b237184ddcc99b89ef1756502459bcd4aa955
tylerselldesign <tylerselldesign@gmail.com>
2019-01-17 21:02:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
     values = []
     
     self.class.column_names.each do |column|
-      values << "'#{send(column)}'"
+      values << "'#{send(column)}'" unless send(column)
   end
   
   def save
