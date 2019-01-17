eace75cd876d3b4b228b97d45f36d609c49893f0
tylerselldesign <tylerselldesign@gmail.com>
2019-01-17 20:46:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -44,10 +44,10 @@ class InteractiveRecord
     SQL
     
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name}")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_names_for_insert}")[0][0]
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
     
   end
   
