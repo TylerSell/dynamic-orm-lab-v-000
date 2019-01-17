64204617b2acf96fbaeca109c2127ed4ac57457b
tylerselldesign <tylerselldesign@gmail.com>
2019-01-17 20:18:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  ATTRIBUTES = {
-    :id => "INTEGER PRIMARY KEY",
-    :name => "TEXT",
-    :grade => "INTEGER"
-  }
+ 
   
   
 end
